6cead3bd6650b6e699c17d58da9a414048f71882
robim16 <carteagajimenez@gmail.com>
2025-10-05 16:25:02 -0500

    creacion del componente location map

--- src/app/landlord/properties-create/step/location-step/country.service.ts
+++ src/app/landlord/properties-create/step/location-step/country.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { State } from '../../../../core/model/state.model';
+import { Country } from './country.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
+  http: HttpClient = inject(HttpClient)
 
-  constructor() { }
+  private countries$: WritableSignal<State<Array<Country>>> = 
+    signal(State.Builder<Array<Country>>().forInit())
+  countries = computed(() => this.countries$())
+
+  private fetchCountry$ = new Observable<Array<Country>>()
+
+  constructor() { 
+    this.initFetchGetAllCountries()
+  }
+
+  initFetchGetAllCountries(): void {
+    this.fetchCountry$ = this.http.get<Array<Country>>("/assets/countries.json")
+      .pipe(
+        tap(countries =>
+          this.countries$.set(State.Builder<Array<Country>>().forSuccess(countries))),
+        catchError(err => {
+          this.countries$.set(State.Builder<Array<Country>>().forError(err));
+          return of(err);
+        }),
+        shareReplay(1)
+      );
+  }
 }
